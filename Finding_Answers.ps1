57ccc71e909f1ed97b4a66a6a0bff8311eb0a02e
Byrd <kbyrd@teksystems.com>
2020-04-19 22:50:32 -0600

    commit

---
+++ Finding_Answers.ps1
@@ -0,0 +1,11 @@
+# Gives information about the help command **ONLY WORKS IN POWERSHELL CORE - NOT VSCODE**
+# This is inforation given within PowerSheell Core itself without external resources
+get-help
+
+get-help | more 
+
+get-help *Service* | more
+
+help get-service
+
+help get-service -Examples
